d943570d70349004db11a902c4b33b466983569c
yaufaniadam <yaufaniadam@yahoo.com>
2023-11-02 00:28:27 +0700

    nota kesepahaman

--- resources/views/user/pages/agreement.blade.php
+++ resources/views/user/pages/agreement.blade.php
@@ -2,11 +2,11 @@
     <section class="py-5" id="features">
         <div class="container px-5 my-5 justify-content-center">
             <h5 class="font-weight-bold mb-5">
-                Syarat dan Persetujuan
+                Nota Kesepahaman
             </h5>
-            <p>
+            <textarea style="width:100%; height:100vh;">
                 {!! $current_agreement->content !!}
-            </p>
+            </textarea>
             <a href="{{ route('user.sign_agreement') }}" class="btn btn-primary">
                 Saya Setuju
             </a>
