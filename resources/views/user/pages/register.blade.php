6cd619ace0a2c441e66c0e702685c87db2930246
yaufaniadam <yaufaniadam@yahoo.com>
2023-11-02 10:41:40 +0700

    label registrais utk nama tpa

--- resources/views/user/pages/register.blade.php
+++ resources/views/user/pages/register.blade.php
@@ -13,7 +13,7 @@
                     </div>
                 @endif
                 <div class="mb-4">
-                    <label for="institution_name" class="form-label">Nama TPA</label>
+                    <label for="institution_name" class="form-label">Nama TPA (Kata 'TPA' tidak perlu ditulis. Contoh: Istiqomah)</label>
                     <input type="text" name="institution_name"
                         class="form-control
                             {{ $errors->has('institution_name') ? 'is-invalid' : '' }}"
