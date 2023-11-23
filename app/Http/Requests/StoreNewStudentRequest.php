f86d5e1f351d2570d6eff9ac8019b5bccee562b7
yaufaniadam <yaufaniadam@yahoo.com>
2023-11-23 09:49:25 +0700

    foto

--- app/Http/Requests/StoreNewStudentRequest.php
+++ app/Http/Requests/StoreNewStudentRequest.php
@@ -70,9 +70,9 @@ class StoreNewStudentRequest extends FormRequest
             'birth_certificate.image' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
             'birth_certificate.mimes' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
             'birth_certificate.max' => 'Ukuran foto tidak boleh lebih dari 1 MB',           
-            // 'photo.image' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
-            // 'photo.mimes' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
-            // 'photo.max' => 'Ukuran foto tidak boleh lebih dari 1 MB',           
+            'photo.image' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
+            'photo.mimes' => 'Akta harus berupa gambar dengan ekstensi .jpg, jpeg atau .png',           
+            'photo.max' => 'Ukuran foto tidak boleh lebih dari 1 MB',           
         ];
     }
 }
