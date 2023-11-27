9c3e71458306dd63bfde59359aaa443b8da643c9
yaufaniadam <yaufaniadam@yahoo.com>
2023-11-27 12:49:25 +0700

    participants

---
+++ resources/views/admin/pages/training/participant-training.blade.php
@@ -0,0 +1,92 @@
+<x-layout>
+    @push('page_css')
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+    @endpush
+
+    <x-slot:title>
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Jumlah peserta saat ini</h1>
+            <a href="{{ route('admin.create_new_training') }}"
+                class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-plus fa-sm text-white-50"></i> Event Baru
+            </a>
+        </div>
+    </x-slot:title>
+
+    @if(session()->has('success'))
+        <div class="alert alert-success" role="alert">
+            {{ session()->get('success') }}
+        </div>
+    @endif
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <table id="dataTable" class="table table-striped" style="width:70%">
+                    <thead>
+                        <tr>
+                            <th style="width: 5%"></th>
+                            <th style="width: 25%">Nama Event</th>
+                            <th style="width: 20%">Peserta</th>
+                            <th style="width: 20%">Keranjang</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($trainings as $training)
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <a href="{{ route('admin.edit_training',$training->id) }}">
+                                        {{ $training->name }}
+                                    </a>
+                                </td>
+                                <td>
+                                    {{ $training->participants_count }}
+                                </td>                               
+                                
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+    @push('page_js')
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+        <!-- Page level custom scripts -->
+        <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+        <script>
+            function confirmSubmit() {
+                var confirmSubmission = confirm(
+                    "Anda yakin ingin menghapus event ini? Event yang sudah dihapus tidak dapat dikembalikan dengan cara apapun."
+                );
+                return confirmSubmission;
+            }
+
+            const table = new DataTable('#dataTable');
+
+            table
+                .on('order.dt search.dt', function () {
+                    let i = 1;
+
+                    table
+                        .cells(null, 0, {
+                            search: 'applied',
+                            order: 'applied'
+                        })
+                        .every(function (cell) {
+                            this.data(i++);
+                        });
+                })
+                .draw();
+
+        </script>
+    @endpush
+
+</x-layout>
