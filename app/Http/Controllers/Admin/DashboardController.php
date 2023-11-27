9a2ef3a0b3c8db0973b3a50ba7958c6d7c5b8f3b
yaufaniadam <yaufaniadam@yahoo.com>
2023-11-27 12:49:03 +0700

    participants count

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Training;
+use App\Models\Cart;
+use App\Services\TransactionService;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,6 +14,15 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.dashboard.index');
+
+        // $trainings = Training::all();
+        $trainings = Training::withCount('participants')->get();
+        $cart = Cart::withCount('items')->get();
+
+        // dd($cart);
+        
+
+        return view('admin.pages.training.participant-training')
+            ->with(compact('trainings', 'cart'));     
     }
 }
